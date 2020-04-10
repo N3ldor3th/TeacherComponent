6dccd5f9b809c88697bc7a7dc8aec4770b0a9bd1
Jan Kuznik <KuznikJan@seznam.cz>
2020-04-10 19:50:14 +0200

    readme + basic swagger setup

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# test
+# build
+to build it with gradle, you can use the gradle wrapper without actually having the gradle installed
+
+./gradlew clean build
+
+# run
+
+./gradlew bootRun
+
+# API documentation
+
+localhost:8080/swagger-ui.html
+
+# test data
+
+as this project is using in-memory H2 db, there is data loaded during the start of the application
+
+# Teacher example in json
+
+{
+    "email": "NovakJan@gmail.com",
+    "skype": "novakjan95",
+    "name": "Jan",
+    "surname": "Novak",
+    "country": "Czech Republic",
+    "languages": [
+        {
+            "code": "en",
+            "name": "English"
+        },
+        {
+            "code": "cs",
+            "name": "Czech"
+        }
+    ],
+    "timeSlots": [
+        {
+            "weekDay": "SATURDAY",
+            "startTime": "2020-04-11T16:09:29.475238",
+            "endTime": "2020-04-11T17:39:29.475238"
+        },
+        {
+            "weekDay": "FRIDAY",
+            "startTime": "2020-04-10T16:09:29.473243",
+            "endTime": "2020-04-10T17:09:29.475238"
+        }
+    ]
+}
+
+# dockerization
+
+FROM openjdk:11-jdk-stretch
+ARG JAR_FILE
+COPY ${JAR_FILE} /
+
+EXPOSE 5000
+
+CMD java $JAVA_OPTS -jar /TeacherComponent.jar
+
+afterwards "docker build" should be enough
+
+
 
--- src/main/java/com/emeldi/teachercomponent/config/SwaggerConfiguration.java
+++ src/main/java/com/emeldi/teachercomponent/config/SwaggerConfiguration.java
@@ -4,16 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        Contact contact = new Contact(
+                "Jan Kuzník",
+                "https://github.com/N3ldor3th",
+                "KuznikJan@seznam.cz"
+        );
+        List<VendorExtension> vendorExtensions = new ArrayList<>();
+        ApiInfo apiInfo = new ApiInfo(
+                "Teacher Component API",
+                "This page documents Teacher Component and its endpoints", "1.0",
+                null, contact, null, null, vendorExtensions);
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
