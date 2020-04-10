9a0970bd872afe50e23ab43c0653692517219c8e
Jan Kuznik <KuznikJan@seznam.cz>
2020-04-10 09:28:12 +0200

    initial commit : basic structure

---
+++ src/main/java/com/emeldi/TeacherComponent/TeacherComponentApplication.java
@@ -0,0 +1,13 @@
+package com.emeldi.TeacherComponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherComponentApplication.class, args);
+	}
+
+}
